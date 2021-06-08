02e676b6199280bb5d91a149053336cbf47c7fb0
Mick <mgrove@gmail.com>
2021-06-08 13:11:32 -0700

    Update install-dotfiles.sh

--- scripts/install-dotfiles.sh
+++ scripts/install-dotfiles.sh
@@ -10,5 +10,8 @@ for file in $(find ./dots/ -name ".*" -not -name ".gitignore" -not -name ".travi
     \cp "$file" "$HOME/$f"
 done
 
+mkdir -p "$HOME/.iterm2"
+\cp "$CURDIR/scripts/com.googlecode.iterm2.plist" "$HOME/.iterm2"
+
 #ln -snf $CURDIR/.bash_profile $HOME/.profile; \
 #ln -snf $CURDIR/zsh/zshrc $HOME/.zshrc;
